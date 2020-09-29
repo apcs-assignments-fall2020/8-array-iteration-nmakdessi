83d66fc446f5249376f341b6dba8e8e2d577d778
nmakdessi <21nmakdessi@athenian.org>
2020-09-29 00:58:16 +0000

    Finished Assignment

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,24 +2,48 @@ public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] newarr = new int[arr.length];
+        int a = 0;
+        for (int i = arr.length -1; i >= 0; i--) {
+            newarr[a] = arr[i];
+            a++;
+        }
+        return newarr;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        int second = arr[0];
+        if (second == max){
+            second = arr[1];
+        
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] != max && arr[i] > second){
+                second = arr[i];
+            }
+        }
+        return second;
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        double x = (double) arr[1] / arr[0];
+        for (int i = 0; i < arr.length-1; i++){
+            if (arr[i] * x != arr[i+1]){
+                return false;
+            }
+        }
+        return true;   
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        
     }
 }
