5f0d405950edcecaadfec5b659f2cc463f0fa307
Nathan Lin <nathanlukelin@gmail.com>
2020-10-09 16:24:27 +0000

    Fixed missing }

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -22,6 +22,7 @@ public class MyMain {
         int second = arr[0];
         if (second == max){
             second = arr[1];
+        }
         
         for (int i = 0; i < arr.length; i++){
             if (arr[i] != max && arr[i] > second){
